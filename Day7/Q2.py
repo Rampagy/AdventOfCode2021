7dcd4a1010f4e356e921088003e02a6d4f85031b
Rampagy <alexjsimon32@gmail.com>
2021-12-06 23:36:44 -0600

    Adding day 7 files

---
+++ Day7/Q2.py
@@ -0,0 +1,27 @@
+def factorialSum(delta):
+    accumulator = 0
+    for i in range(delta):
+        accumulator += (i + 1)
+    return accumulator
+
+if __name__ == '__main__':
+    horizontal_positions = []
+    with open('input.txt', 'r') as f:
+        for line_count, line in enumerate(f):
+            horizontal_positions = line.split(',')
+    
+    positions = []
+    for pos in horizontal_positions.copy():
+        positions += [int(pos)]
+    del horizontal_positions
+
+    min_fuel = 99999999999999999999999999
+    for i in range(max(positions)):
+        alist = [i]*(len(positions))
+
+        current_fuel = sum([factorialSum(abs(a - b)) for a, b in zip(alist, positions)])
+
+        if current_fuel < min_fuel:
+            min_fuel = current_fuel
+
+    print(min_fuel)

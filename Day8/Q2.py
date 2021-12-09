a58a69122a96ae2221032edbe26082235ba53689
Rampagy <alexjsimon32@gmail.com>
2021-12-08 18:08:30 -0600

    Adding day 8 code

--- Day8/Q2.py
+++ Day8/Q2.py
@@ -7,8 +7,8 @@ def replaceChars(theString, lettersToRemove):
 
 
 if __name__ == '__main__':
-
-    with open('input1.txt', 'r') as f:
+    solution = 0
+    with open('input.txt', 'r') as f:
         for line_count, line in enumerate(f):
             ins, outs = line.split('|')
 
@@ -21,15 +21,15 @@ if __name__ == '__main__':
             for item in inputs:
                 if len(item) == 2:
                     # this item must be a 1
-                    solved_words[item] = 1
+                    solved_words[item] = [1]
 
                 if len(item) == 3:
                     # this item must be 7
-                    solved_words[item] = 7
+                    solved_words[item] = [7]
 
                 if len(item) == 4:
                     # this item must be a 4
-                    solved_words[item] = 4
+                    solved_words[item] = [4]
 
                 if len(item) == 5:
                     solved_words[item] = [2, 3, 5]
@@ -39,54 +39,105 @@ if __name__ == '__main__':
 
                 if len(item) == 7:
                     # this item must be a 8
-                    solved_words[item] = 8
+                    solved_words[item] = [8]
 
-            eight_item = [key for key, val in solved_words.items() if val == 8 ][0]
-            seven_item = [key for key, val in solved_words.items() if val == 7 ][0]
-            one_item   = [key for key, val in solved_words.items() if val == 1 ][0]
+            one_item   = [key for key, val in solved_words.items() if 1 in val][0]
+            four_item  = [key for key, val in solved_words.items() if 4 in val][0]
+            seven_item = [key for key, val in solved_words.items() if 7 in val][0]
+            eight_item = [key for key, val in solved_words.items() if 8 in val][0]
 
-            for segment in ['a', 'e', 'c', 'd', 'f', 'b', 'g']:
-                
+            for segment in ['a', 'g', 'd', 'b', 'e', 'c', 'f']:
                 for key, val in solved_words.items():
-                    if set(key) < set(seven_item) and val == 1:
-                        # 'a' (7 - 2)
+
+                    if set(key) < set(seven_item) and 1 in val and 'a' not in solved_letters and segment == 'a':
+                        # 'a' (7 - 1)
                         solved_letters[segment] = replaceChars(seven_item, key)
                         break
 
-                    if key in eight_item and 9 in [val] and key is not eight_item:
+                    if set(four_item) < set(key) and 9 in val and 'g' not in solved_letters and 'a' in solved_letters and segment == 'g':
+                        # 'g' (9 - 4 - 'a')
+                        solved_letters[segment] = replaceChars(key, four_item+solved_letters['a'])
+                        break
+
+                    if set(one_item) < set(key) and 3 in val and 'd' not in solved_letters and 'a' in solved_letters and 'g' in solved_letters and segment == 'd':
+                        # 'd' (3 - 1 - 'a' - 'g')
+                        solved_letters[segment] = replaceChars(key, one_item+solved_letters['a']+solved_letters['g'])
+                        break
+
+                    if 'd' in solved_letters and 'b' not in solved_letters and segment == 'b':
+                        # 'b' (4 - 1 - 'd')
+                        solved_letters[segment] = replaceChars(four_item, one_item+solved_letters['d'])
+                        break
+
+                    if set(four_item) < set(key) and 9 in val and 'e' not in solved_letters and segment == 'e':
                         # 'e' (8 - 9)
                         solved_letters[segment] = replaceChars(eight_item, key)
                         break
 
-                    if key in eight_item and 6 in [val] and key is not eight_item:
-                        # 'c' (8 - 6)
-                        solved_letters[segment] = replaceChars(eight_item, key)
+                    if 2 in val and 'a' in solved_letters and 'd' in solved_letters and \
+                            'e' in solved_letters and 'g' in solved_letters and \
+                            solved_letters['a'] in key and solved_letters['d'] in key and \
+                            solved_letters['e'] in key and solved_letters['g'] in key and \
+                            'c' not in solved_letters and segment == 'c':
+                        # 'c' (2 - 'a' - 'd' - 'e' - 'g')
+                        solved_letters[segment] = replaceChars(key, solved_letters['a']+solved_letters['d']+solved_letters['e']+solved_letters['g'])
                         break
 
-                    if key in eight_item and 0 in [val] and key is not eight_item:
-                        # 'd' (8 - 0)
-                        solved_letters[segment] = replaceChars(eight_item, key)
+                    if 'c' in solved_letters and 'f' not in solved_letters and segment == 'f':
+                        # 'f' (1 - 'c')
+                        solved_letters[segment] = replaceChars(one_item, solved_letters['c'])
                         break
 
-                    if val == 7 and 'a' in key and 'c' in key:
-                        # 'f' (7 - 'a' - 'c')
-                        solved_letters[segment] = replaceChars(key, 'ac')
-                        break
-
-                    if val == 4 and 'd' in solved_letters:
-                        # 'b' (4 - 1 - 'd')
-                        solved_letters[segment] = replaceChars(key, one_item+solved_letters['d'])
-                        break
-
-                    if set(key) < set(eight_item) and val == 4 and 'a' in solved_letters and 'd' in solved_letters:
-                        # 'g' (8 - 4 - 'a' - 'e')
-                        solved_letters[segment] = replaceChars(eight_item, key+solved_letters['a']+solved_letters['e'])
-                        break
-
+            # swap key and value
+            solved_letters = { val: key for key, val in solved_letters.items() }
 
             # sort the dictionary
             solved_letters = dict(sorted(solved_letters.items()))
-            print(solved_letters)
+
+            multiplier = 1000
+            accumulator = 0
+            for word in outputs:
+                if len(word) == 2:
+                    accumulator += int(1 * multiplier)
+                
+                elif len(word) == 3:
+                    accumulator += int(7 * multiplier)
+
+                elif len(word) == 4:
+                    accumulator += int(4 * multiplier)
+
+                elif len(word) == 5:
+                    new_word = ''
+                    for letter in word:
+                        new_word += solved_letters[letter]
+
+                    if set('acdeg') == set(new_word):
+                        accumulator += int(2 * multiplier)
+                    elif set('acdfg') == set(new_word):
+                        accumulator += int(3 * multiplier)
+                    elif set('abdfg') == set(new_word):
+                        accumulator += int(5 * multiplier)
+
+                elif len(word) == 6:
+                    new_word = ''
+                    for letter in word:
+                        new_word += solved_letters[letter]
+
+                    if set('abcefg') == set(new_word):
+                        accumulator += int(0 * multiplier)
+                    elif set('abdefg') == set(new_word):
+                        accumulator += int(6 * multiplier)
+                    elif set('abcdfg') == set(new_word):
+                        accumulator += int(9 * multiplier)
+
+                elif len(word) == 7:
+                    accumulator += int(8 * multiplier)
+
+                multiplier /= 10
+
+            solution += accumulator
+
+    print(solution)
 
 
 

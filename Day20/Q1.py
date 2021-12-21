68c1d69e782cdff7f61fc4e24baf404714228ae3
Rampagy <alexjsimon32@gmail.com>
2021-12-20 22:52:17 -0600

    Adding Day 20

---
+++ Day20/Q1.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+def generate_new_picture(picture, img_enhance_alg, iteration):
+    new_picture = []
+    expanded_pic = np.pad(picture, 1)
+
+    for row_num, row in enumerate(expanded_pic): # keep in mind this is numpy array now
+        new_row = []
+        for col_num, pix in enumerate(row):
+            img_enhance_alg_num = 0
+            bit_shift = 8
+            for row_mod in range(-1, 2):
+                for col_mod in range(-1, 2):
+                    img_enhance_val = 0
+                    if iteration % 2 != 0 and iteration != 0:
+                        # change default
+                        img_enhance_val = 1
+
+                    if row_num+row_mod >= 0 and row_num+row_mod < len(expanded_pic[0]) and \
+                            col_num+col_mod >= 0 and col_num+col_mod < len(expanded_pic):
+                        img_enhance_val = expanded_pic[row_num+row_mod][col_num+col_mod]
+
+                    img_enhance_alg_num |= (img_enhance_val << bit_shift)
+                    bit_shift -= 1
+
+            # once we know the img enhance alg num, look it up and determine the new output pixel
+            new_row += [img_enhance_alg[img_enhance_alg_num]]
+        new_picture += [new_row]
+
+    return new_picture
+
+if __name__ == '__main__':
+    with open('input1.txt', 'r') as f:
+        img_enhance_alg = []
+        picture = []
+        buildPictureFlag = False
+        for line_count, line in enumerate(f):
+            row = []
+            if not buildPictureFlag:
+                for char in line:
+                    if char != '\n' and char != ' ' and char != 0x9:
+                        if char == '#':
+                            img_enhance_alg += [1]
+                        else:
+                            img_enhance_alg += [0]
+                    elif char == '\n' and line == '\n':
+                        # transition to building picture
+                        buildPictureFlag = True
+            else:
+                # build picture
+                for char in line:
+                    if char != '\n' and char != ' ' and char != 0x9:
+                        if char == '#':
+                            row += [1]
+                        else:
+                            row += [0]
+
+                if row != []:
+                    picture += [row]
+
+    for i in range(2):
+        picture = generate_new_picture(picture, img_enhance_alg, i)
+        print(len(picture), len(picture[0]))
+
+    accum = 0
+    for row in picture:
+        for num in row:
+            accum += num
+
+    print(accum)
+
+
+
+
+
---
+++ Day20/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+
+b = np.pad(a, 1)
